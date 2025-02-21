74bb1f697bd4795768a0d11fefdcda74cfd0853b
Rupesh Biswas <60888753+rupesh-biswas@users.noreply.github.com>
2025-02-21 22:58:11 +0530

    updated webpage resume details and css

--- src/constants/resumeConstants.ts
+++ src/constants/resumeConstants.ts
@@ -11,7 +11,7 @@ export const education: resumeConstant = {
   subTitle: "Bachelor of Technology",
   startDate: 2015,
   endDate: 2019,
-  description: ["Graduated with first class degree"],
+  description: ["Graduated with first class degree (CGPA: 7.42)"],
 };
 
 export const experience: resumeConstant[] = [
@@ -21,11 +21,12 @@ export const experience: resumeConstant[] = [
     startDate: "Jul 2023",
     endDate: "Present",
     description: [
-      "Ensured correct access to Power BI datasets across customer environments and delivered insights for 4+ clients.",
-      "Created 3+ templatized Power BI files with DAX formulas, helping CS teams obtain preliminary insights within 3 days of deployment. ",
-      "Wrote all data load scripts in QlikView using SQL and Qlik scripting to deliver custom insights for a Fortune 500 client.",
-      "Built and hosted a React website to demo the company’s Power BI offerings, helping the team to demo effectively.",
-      "Created insights for a fortune 500 country by extracting identifiers from the DOM collected across users and mapped them to work sequences to provide insights on how users.",
+      "Designed and managed Power BI datasets, ensuring correct data model joins and cardinality across customer environments.",
+      "Created 3+ standardized Power BI files with DAX formulas, accelerating insights generation within 3 days of deployment.",
+      "Built SQL-based data ingestion pipelines in QlikView, delivering insights for a Fortune 500 client.",
+      "Created a React-based website to showcase Power BI capabilities, improving client demo efficiency.",
+      "Developed a Python-based extraction pipeline to extract work identifiers from the collected DOM data across users, mapping them to work sequences to generate insights at the unit-of-work level.",
+      "Contributed to the creation of multiple sales demo proposals by adding relevant data points and product screenshots to showcase capabilities.",
     ],
   },
   {
@@ -34,9 +35,9 @@ export const experience: resumeConstant[] = [
     startDate: "Jan 2022",
     endDate: "Jul 2023",
     description: [
+      "Automated data-driven DevOps workflows by developing a serverless app for Azure DevOps ticketing from Datadog alerts.",
       "Managed and successfully delivered QuickStart client engagement for 6+ customers.",
-      "Developed a serverless app to create Azure DevOps tickets from Datadog alerts.",
-      "Conducted custom analysis using Python and SQL on company data to report KPI performance for a Fortune 500 client.",
+      "Conducted custom analysis using Python and SQL, optimizing KPI reporting for a Fortune 500 client.",
       "Created competitive analysis slide decks for the sales team to do objection handling effectively.",
       "Integrated the learning management system for the partner and customers to enable them about the product.",
       "Developed guided demo journeys with Reprise for various industries, reducing reliance on the demo portal.",
@@ -48,9 +49,8 @@ export const experience: resumeConstant[] = [
     startDate: "Nov 2020",
     endDate: "Dec 2021",
     description: [
-      "Owned the product roadmap and executed all the requirements of the content team to make their processes more streamlined.",
-      "Planned, documented, and implemented multiple features on the website like the dropdown menu, website's tech stack changes to react, currency switcher for abroad colleges.",
-      "Planned and executed the current Q&A feature on the website from scratch.",
+      "Led data-driven product roadmap execution, enhancing web UI and analytics capabilities.",
+      "Planned, documented, and implemented multiple website features, including migration to React, a user path selection navigation menu, a Q&A page, and a currency switcher for abroad colleges.",
       "Launched the current version of Collegedunia mobile app, increasing DAU by 20%.",
       "Executed technical requirements for the WordPress blog.",
     ],
@@ -61,10 +61,10 @@ export const experience: resumeConstant[] = [
     startDate: "Jul 2019",
     endDate: "Oct 2020",
     description: [
-      "Create various reports and insights on the website traffic using google analytics. Built the reports in Google data studio offering and was used at leadership level for daily reporting.",
-      "Used python to scrape more than 20+ websites to get its data.",
-      "Worked with interns to create a notification dashboard for a 5-member team.",
-      "Used screaming frog tool to report the various link building errors and website bugs and got them fixed.",
+      "Developed traffic analytics reports using Google Analytics and Looker Studio (formerly Google Data Studio) for leadership reporting.",
+      "Automated data extraction from 20+ websites using Python, scheduling jobs with cron to fetch updated data at different intervals for competitor analysis and content optimization.",
+      "Managed interns to create a notification dashboard for a 15-member team.",
+      "Identified and resolved SEO issues, including 404 pages and link-building errors using Screaming Frog for improved search rankings.",
       "Setup Google analytics tracking for Prepp.in domain, a completely new website.",
     ],
   },
